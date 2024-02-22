67809d70d3b2ab808761f3545dd6d5d5298cf6df
excel-asaph <excelasaph@gmail.com>
2024-02-22 11:10:21 +0200

    added first and second variables to the script

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstCard = 12;
+let secondCard = 9;
+let sum = firstCard + secondCard;
+
+if (sum < 21) {
+    console.log("Do you want to draw a mew card? 🙂");
+} else if (sum === 21) {
+    console.log("Wohoo! You've got Blackjack! 🥳");
+} else {
+    console.log("You're out of the game! 😭")
+}
